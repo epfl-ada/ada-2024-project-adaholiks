921ccacb0a88f158b81e93c10299cd6cdfaa033d
Fluntch <fletcher.collis@epfl.ch>
2024-11-08 13:55:51 +0100

    Part1 floops

--- src/graph_data.py
+++ src/graph_data.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')
 
 print(sys.path)
 
-from load_data import load_article_objects
+from src.load_data import load_article_objects
 import networkx as nx
 import matplotlib.pyplot as plt
 from pyvis.network import Network
@@ -31,27 +31,27 @@ for article_name, article_obj in articles.items():
 # Now G is the NetworkX graph with articles as nodes and links as edges
 
 # Example: print nodes and edges
-# print("Nodes in the graph:")
-# print(G.nodes(data=True))
+#print("Nodes in the graph:")
+#print(G.nodes(data=True))
 
-# print("\nEdges in the graph:")
-# print(G.edges())
+#print("\nEdges in the graph:")
+#print(G.edges())
 
 
 # Choose a layout for the graph (spring layout is often a good default)
-pos = nx.spring_layout(G, k=0.15, iterations=20)
+#pos = nx.spring_layout(G, k=0.15, iterations=20)
 
 # Create the figure and plot the graph
-plt.figure(figsize=(12, 12))  # Adjust the figure size based on the graph size
+#plt.figure(figsize=(12, 12))  # Adjust the figure size based on the graph size
 
 # Draw the nodes with labels and edges
-nx.draw(G, pos, with_labels=True, node_size=500, node_color="skyblue", font_size=8, font_weight="bold", edge_color="gray")
+#nx.draw(G, pos, with_labels=True, node_size=500, node_color="skyblue", font_size=8, font_weight="bold", edge_color="gray")
 
 # Display the graph
-plt.show()
+#plt.show()
 
-net = Network(notebook=True, cdn_resources='remote', height="750px", width="100%")
-print("created network")
-net.from_nx(G)  # Load NetworkX graph into pyvis
-print("loaded graph")
-net.show("graph.html")  # This will open a zoomable, interactive graph in your browser
+#net = Network(notebook=True, cdn_resources='remote', height="750px", width="100%")
+#print("created network")
+#net.from_nx(G)  # Load NetworkX graph into pyvis
+#print("loaded graph")
+#net.show("graph.html")  # This will open a zoomable, interactive graph in your browser
